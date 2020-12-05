ad6820fc3b9dc7a938986356162ef1cd38ff450d
caloloc2 <calolomino@gmail.com>
2020-12-04 22:48:14 -0500

    actualiza

--- js/ajax.js
+++ js/ajax.js
@@ -1,5 +1,5 @@
-var server = 'http://localhost/camarafrio/'; 
-// var server = 'http://aloja.vtgsa.com';
+// var server = 'http://localhost/camarafrio/'; 
+var server = 'http://camarafrio.nibemi.com/';
 
 function Consulta_Temperaturas(fields, callback){
     $.ajax({
